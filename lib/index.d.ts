c151dcb2377f34b9409e69483971ffca342fd4b2
Erich Kuba <ekuba@appsolute.nz>
2020-11-28 13:13:53 +1300

    Removed the lib folder as it is easily recreated by the build process

--- lib/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { append, areEqual, clone, extractAndRedact, hasProperty, isArray, isBoolean, isDate, isEmpty, isError, isNumber, isObject, isRegExp, isString, isDefined, isUndefined, stringify, } from './utils';
